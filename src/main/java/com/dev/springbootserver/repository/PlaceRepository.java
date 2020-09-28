802a30d2fa79edf257520a0a9a7d7633d6cd8c2c
caio-k <koiti2000@hotmail.com>
2020-09-28 17:40:22 -0300

    added repository annotation

--- src/main/java/com/dev/springbootserver/repository/PlaceRepository.java
+++ src/main/java/com/dev/springbootserver/repository/PlaceRepository.java
@@ -3,10 +3,12 @@ package com.dev.springbootserver.repository;
 import com.dev.springbootserver.model.Place;
 import com.dev.springbootserver.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     boolean existsByNameAndSchoolId(String name, Long schoolId);
